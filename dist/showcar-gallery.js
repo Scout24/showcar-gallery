71cfab8c28125f7a13c75cccd1bf6b025d8b1d65
m-idler <marcel.idler@mayflower.de>
2015-12-02 12:56:44 +0100

    create base for gallery

--- dist/showcar-gallery.js
+++ dist/showcar-gallery.js
@@ -44,9 +44,39 @@
 /* 0 */
 /***/ function(module, exports) {
 
-	"use strict";
+	'use strict';
 	
-	$(document).ready(function () {});
+	$(document).ready(function () {
+	
+	    function paginate(direction) {
+	        $('.gallery-item').each(function () {
+	            var left = parseInt($(this).css('left'));
+	            $(this).css('left', left + direction);
+	        });
+	        if (direction > 0) {
+	            //left clicked, move last item to the beginning
+	            var last = $('.gallery-item').last();
+	            last.hide();
+	            $('.gallery-wrapper').prepend(last);
+	            last.css('left', -300);
+	            last.show();
+	        } else if (direction < 0) {
+	            //right clicked, move first item to the end
+	            var first = $('.gallery-item').first();
+	            first.hide();
+	            $('.gallery-wrapper').append(first);
+	            first.css('left', 500);
+	            first.show();
+	        }
+	    }
+	
+	    $('#left').click(function () {
+	        paginate(200);
+	    });
+	    $('#right').click(function () {
+	        paginate(-200);
+	    });
+	});
 
 /***/ }
 /******/ ]);
