d969c60bc8c628386efc40bc3429d654fa51797b
m-idler <marcel.idler@mayflower.de>
2016-02-29 10:44:30 +0100

    fix small typo bug in safari

--- src/js/showcar-gallery.js
+++ src/js/showcar-gallery.js
@@ -161,7 +161,7 @@ var as24gallery = Object.assign(Object.create(HTMLElement.prototype), {
                 var dataNo = i % noOfItems;
                 dataNo = dataNo || noOfItems;
                 index += 1;
-                var el = $('[data-number="' + dataNo + '"').clone().data('number', index).addClass(this.duplicateClass);
+                var el = $('[data-number="' + dataNo + '"]').clone().data('number', index).addClass(this.duplicateClass);
                 var target = $('[data-number="' + (index - 1) + '"]');
                 target.after(el);
             }
