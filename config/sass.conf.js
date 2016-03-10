27a060c48cb33828999a0276fdf88ecd2dced73a
Florian Fackler <florian.fackler@mintao.com>
2016-03-10 16:15:34 +0100

    Enrico Carousel

--- config/sass.conf.js
+++ config/sass.conf.js
@@ -9,9 +9,11 @@ module.exports = (function () {
             sourceMapEmbed: true
         },
         files: [
-            {dest: 'examples/css/style.css', src: 'examples/css/style.scss'},
-            {dest: 'dist/showcar-gallery.css', src: 'src/scss/showcar-gallery.scss'},
-            {dest: 'dist/showcar-carousel.css', src: 'src/scss/showcar-carousel.scss'},
+            {src: 'src/scss/showcar-carousel.scss', dest: 'dist/showcar-carousel.css'},
+            {src: 'src/scss/showcar-gallery.scss', dest: 'dist/showcar-gallery.css'},
+            {src: 'examples/carousel/css/style.scss', dest: 'examples/carousel/css/style.css'},
+            {src: 'examples/gallery/css/style.scss', dest: 'examples/gallery/css/style.css'},
+            {src: 'examples/css/style.scss', dest: 'examples/css/style.css'},
             {
                 expand: true,
                 cwd: 'examples',
